8b29284c30f46558381e37afb87b4d4ec65a3fc6
白石洋輝 <hirokishiraishi@HS.local>
2020-10-19 04:01:48 +0900

    Revised

--- classifier.py
+++ classifier.py
@@ -1,7 +1,5 @@
 import numpy
 import numpy.random
-import itertools
-from parameters import Parameters
 
 """
 A classifier in XCSR
@@ -12,10 +10,10 @@ class Condition:
         self.c = center
         self.s = spread
 
-    def _get_lower_bound(self):
+    def get_lower_bound(self):
         return self.c - self.s
 
-    def _get_upper_bound(self):
+    def get_upper_bound(self):
         return self.c + self.s
 
 class Classifier:
@@ -46,6 +44,7 @@ class Classifier:
             self.condition.append(cl_condition)
             self.condition_c.append(cl_condition.c)
             self.condition_s.append(cl_condition.s)
+        #print(Classifier.global_id,self.condition_c,self.condition_s)
 
     def __str__(self):
         return "Classifier " + str(self.id) + ": " + self.condition + " = " + str(self.action) + " Fitness: " + str(self.fitness) + " Prediction: " + str(self.prediction) + " Error: " + str(self.error) + " Experience: " + str(self.experience)
@@ -56,6 +55,7 @@ class Classifier:
        @param state - The state of the system to mutate around
        @param mu - The probability with which to mutate
        @param num_actions - The number of actions in the system
+    """
     """
     def _apply_mutation(self, state, mu, num_actions):
         for i in range(len(self.condition)):
@@ -72,7 +72,19 @@ class Classifier:
         
         if numpy.random.rand() < mu:
             self.action = numpy.random.randint(0, num_actions)
+    """
+    def _apply_mutation(self, state, mu, num_actions):
+        for i in range(len(self.condition)):
+            if numpy.random.rand() < mu:
+                if numpy.random.rand() < 0.5:
+                    self.condition[i].c += 2 * self.m * numpy.random.rand() - self.m
+                    self.condition[i].c = min(max(0.0, self.condition[i].c), 1.0)
+                else:
+                    self.condition[i].s += 2 * self.m * numpy.random.rand() - self.m
+                    self.condition[i].s = max(0.0, self.condition[i].s)
 
+        if numpy.random.rand() < mu:
+            self.action = numpy.random.randint(0, num_actions)
     """
     DELETION VOTE (3.11 Deletion from the population ~The deletion vote~)
        Calculates the deletion vote for this classifier, that is, how much it thinks it should be deleted
@@ -104,10 +116,10 @@ class Classifier:
     def _is_more_general(self, spec):
         k = 0
         for i in range(len(self.condition)):
-            l_gen = self.condition[i]._get_lower_bound()
-            u_gen = self.condition[i]._get_upper_bound()
-            l_spec = spec.condition[i]._get_lower_bound()
-            u_spec = spec.condition[i]._get_upper_bound()
+            l_gen = self.condition[i].get_lower_bound()
+            u_gen = self.condition[i].get_upper_bound()
+            l_spec = spec.condition[i].get_lower_bound()
+            u_spec = spec.condition[i].get_upper_bound()
             if l_spec < l_gen or u_gen < u_spec:
                 return False
             if l_spec == l_gen and u_gen == u_spec:
